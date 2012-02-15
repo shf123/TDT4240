b562b1ef8c0c9d77c5d1690d00cc134e63d88199
Anders Sandvik <andersolsensandvik@gmail.com>
2012-02-15 23:56:18 +0100

    Hello github

--- TDT4240android/src/net/andersos/TDT4240android/TDT4240androidActivity.java
+++ TDT4240android/src/net/andersos/TDT4240android/TDT4240androidActivity.java
@@ -9,5 +9,6 @@ public class TDT4240androidActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        // HEllo world
     }
 }
