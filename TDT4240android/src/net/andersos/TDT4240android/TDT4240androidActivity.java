0fb48624fffce2c23ec903b53d5625626d1702a9
Anders Sandvik <andersolsensandvik@gmail.com>
2012-02-15 23:55:51 +0100

    commiting with files

---
+++ TDT4240android/src/net/andersos/TDT4240android/TDT4240androidActivity.java
@@ -0,0 +1,13 @@
+package net.andersos.TDT4240android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TDT4240androidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
